1646a7e5db88bd0b16d3cadd3663c16d4a1f2c04
sravanidurgam <sravanidurgam758@gmail.com>
2024-05-20 12:51:31 +0530

    Initial commit

---
+++ src/shoppingfolder/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='bannerSection'>
+      <div className="bannerBox">
+        <img src="assets/ban1.jpg" alt="banner" />
+      </div>
+      <div className="bannerBox">
+        <img src="assets/l2.gif" alt="banner" />
+      </div>
+
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/shoppingfolder/data.js
@@ -0,0 +1,32 @@
+export const Gents = {
+    title: "Gents Fashion",
+    image1: "assets/Men/22.jpg",
+    image2: "assets/Men/24.jpg",
+    image3: "assets/Men/3.jpg",
+    image4: "assets/Men/38.jpg",
+    image5: "assets/Men/43.jpg",
+    image6: "assets/Men/33.jpg",
+    price1: "559 INR",
+    price2: "459 INR",
+    price3: "659 INR",
+    price4: "759 INR",
+    price5: "859 INR",
+    price6: "959 INR",
+
+}
+export const Ladies = {
+    title: "Ladies Fashion",
+    image1: "assets/Woman/11.jpg",
+    image2: "assets/Woman/44.jpg",
+    image3: "assets/Woman/3.jpg",
+    image4: "assets/Woman/22.jpg",
+    image5: "assets/Woman/33.jpg",
+    image6: "assets/Woman/8.jpg",
+    price1: "1559 INR",
+    price2: "1459 INR",
+    price3: "1659 INR",
+    price4: "1759 INR",
+    price3: "1859 INR",
+    price4: "1959 INR",
+
+}
